e9a7343d41783e688315498694e2b9857a87435c
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-05 18:30:21 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -24,7 +24,7 @@ def update_status
   # Code your solution here:
 
   epic_tragedy[:montague][:hero][:status] = 'dead'
-  epic_tragedy[:capulet][:hero][:name][:age][:status] = 'dead'
+  epic_tragedy[:capulet][:hero][:status] = 'dead'
 
 
 
